30b6c80e3c4ae7dcc248d7e2d8d29fef9b895d3c
Vladi <you@example.com>
2026-03-13 11:02:52 +0100

    hallo

--- pages/1_About.py
+++ pages/1_About.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from openai import OpenAI
 
 
 im = 'images/favicon.png'
@@ -12,3 +13,36 @@ st.set_page_config(
 st.title("About")
 
 st.write("You have discovered FellX! It is a analytical programm for XRD data. The programm comes with visual analysis as well as curve fitting and for example grain size and scherrer and Willamson and Hall.")
+
+
+
+st.title("ChatGPT-like clone")
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
